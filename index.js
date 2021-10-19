91cb9d6cba319e2adcf211698e61337dda9fb553
RaisaKhaliulina <92810216+RaisaKhaliulina@users.noreply.github.com>
2021-10-20 00:14:31 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const element = <h1>Hello World.
+Меня зовут:  Раиса Халиулина.
+</h1>
+
+ReactDom.render(element,document.getElementById('root'))
